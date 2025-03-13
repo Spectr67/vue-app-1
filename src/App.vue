87e27c291ebd9f577be4ab07f141bcbbd51d7fe5
Arthur <pennywennyweed@gmail.com>
2025-03-13 23:46:53 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Vue',
+        count: 0,
+        cars: ['bmw', 'mercedess', 'audi'],
+      }
+    },
+  }
+</script>
+
+<template>
+  <div class="main">
+    <h1>hello {{ count }}</h1>
+    <textarea v-bind:placeholder="message"></textarea>
+    <button v-on:click="cars.push(message)">click me</button>
+    <ul>
+      <li v-for="(car, idx) of cars" :key="idx">{{ car }}</li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
