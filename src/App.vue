cd551a3a7b9a1fa8b88e605924bc1e9428dad2d2
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-13 23:57:33 +0200

    Added v-on

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script>
-  export default {
-    data() {
-      return {
-        message: 'Vue',
-        count: 0,
-        cars: ['bmw', 'mercedess', 'audi'],
-      }
-    },
-  }
+export default {
+  data() {
+    return {
+      message: 'Vue',
+      count: 0,
+      cars: ['bmw', 'mercedess', 'audi'],
+    }
+  },
+}
 </script>
 
 <template>
   <div class="main">
     <h1>hello {{ count }}</h1>
-    <textarea v-bind:placeholder="message"></textarea>
+    <textarea v-on:change="cars.push($event.target.value)"></textarea>
     <button v-on:click="cars.push(message)">click me</button>
     <ul>
       <li v-for="(car, idx) of cars" :key="idx">{{ car }}</li>
