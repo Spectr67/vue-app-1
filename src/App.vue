74de1cb08ca0d1fab5c2f6629a83bac85fe9c18f
Arthur <pennywennyweed@gmail.com>
2025-03-18 23:36:51 +0200

    add object cars

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <script>
-export default {
-  data() {
-    return {
-      message: 'Vue',
-      count: 0,
-      cars: ['bmw', 'mercedess', 'audi'],
-    }
-  },
-}
+  export default {
+    data() {
+      return {
+        brand: '',
+        price: 0,
+        count: 0,
+        cars: {
+          // brands: ['bmw', 'audi', 'mercedes'],
+          BMW: 22000,
+          mercedes: 23000,
+          audi: 24000,
+        },
+      }
+    },
+  }
 </script>
 
 <template>
   <div class="main">
     <h1>hello {{ count }}</h1>
-    <textarea v-on:change="cars.push($event.target.value)"></textarea>
-    <button v-on:click="cars.push(message)">click me</button>
+    <textarea v-on:change="brand = $event.target.value">brand</textarea>
+    <textarea v-on:change="price = $event.target.value">price</textarea>
+    <button v-on:click=";(cars[brand] = price), (brand = ''), (price = 0)">
+      click me
+    </button>
     <ul>
-      <li v-for="(car, idx) of cars" :key="idx">{{ car }}</li>
+      <li v-for="(car, key) in cars" :key="key">{{ key }}: {{ car }}</li>
     </ul>
   </div>
 </template>
